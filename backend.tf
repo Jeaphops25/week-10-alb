355ce1b687ae7b5f5409319b933c0ddb17ab3ea9
Jeaph <jeaph.titalom@gmail.com>
2025-05-05 00:22:06 +0200

    little update 2

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-s3-jeaph"
+    bucket  = "replace here with your bucket name"
     key     = "alb/terraform.state"
     region  = "us-east-1"
     encrypt = true
